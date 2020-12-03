09627669ee2881635a2675b4def76d73d89f3785
Mikolaj Dobrucki <5467602+mikolajdobrucki@users.noreply.github.com>
2020-12-03 21:43:17 +0100

    Build custom Controller component

--- web/src/components/controller.js
+++ web/src/components/controller.js
@@ -1,13 +1,52 @@
+import {Card, Flex, Heading, Label} from '@sanity/ui'
 import React from 'react'
+import styled from 'styled-components'
+import ControllerIcon from '../components/controllerIcon'
+import {hues, white} from '@sanity/color'
 
-import styles from './controller.module.css'
+const ControllerLabel = styled(Label)`
+  text-align: center;
+  padding-bottom: 10px;
+  max-width: 72px;
+`
+
+const ControllerBlock = styled.label`
+  border-radius: 10px;
+  margin: 2px;
+  &:hover {
+    background: ${hues.gray[50].hex};
+  }
+  background: ${props => (props.active ? hues.gray[50].hex : '')};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const Range = styled.input`
+width: 100%;
+margin-top: 30px;
+&::-webkit-slider-runnable-track {
+  height: 2px;
+  border: 0;
+}
+&[type=range]::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  border-radius: 50%;
+  height: 8px;
+  width: 8px;
+  margin-top: -6px;
+  background: ${hues.blue[500].hex};
+  border: 1px solid ${hues.blue[500].hex};
+  box-shadow: 0px 1px 1px rgba(0, 0, 0, 0.1), 0px 4px 4px rgba(0, 0, 0, 0.1), inset 0px -2px 2px rgba(0, 30, 185, 0.25), inset 0px -1px 1px rgba(0, 30, 185, 0.25), inset 0px 1px 1px rgba(255, 255, 255, 0.5), inset 0px 2px 2px rgba(255, 255, 255, 0.5);
+}
+`
 
 const Controller = props => {
   return (
-    <div className={styles.root}>
-      <h3 className={styles.title}>{props.title}</h3>
-      { props.type === 'range' ? (
-        <input
+    <Card paddingY={3} paddingX={4}>
+      <Heading as='h3'>{props.title}</Heading>
+      {props.type === 'range' ? (
+        <Range
           type='range'
           name={props.id}
           id={props.id}
@@ -15,25 +54,33 @@ const Controller = props => {
           max={500}
           step={25}
           value={props.state}
-          onChange={(e) => props.onChange(parseInt(e.target.value))}
+          onChange={e => props.onChange(parseInt(e.target.value))}
         />
       ) : (
-        props.values.map((value) => (
-          <div className='input' key={value}>
-            <label>
-              <input
-                type='radio'
-                name={props.id}
-                id={props.id}
-                checked={props.state === value}
-                onChange={() => props.onChange(value)}
-              />
-              {value}
-            </label>
-          </div>
-        ))
+        <Card marginY={2}>
+          <Flex wrap='wrap'>
+            {props.values.map(value => (
+              <ControllerBlock key={value} active={props.state === value}>
+                <ControllerIcon icon={props.id + '-' + value} active={props.state === value} value={value} />
+                <input
+                  type='radio'
+                  name={props.id}
+                  id={props.id}
+                  checked={props.state === value}
+                  onChange={() => props.onChange(value)}
+                  style={{appearance: 'none', position: 'absolute'}}
+                />
+                { typeof value !== 'number' ? (
+                  <ControllerLabel muted={props.state !== value} size={1}>
+                    {value}
+                  </ControllerLabel>
+                ) : ' '}
+              </ControllerBlock>
+            ))}
+          </Flex>
+        </Card>
       )}
-    </div>
+    </Card>
   )
 }
 
