cca7e598a482432ef9faa0e89ede9e4b7d09d8f4
Mikolaj Dobrucki <5467602+mikolajdobrucki@users.noreply.github.com>
2020-12-04 10:59:57 +0100

    Update gatsby and add missing styled-components plugins

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -11,6 +11,7 @@ const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
   plugins: [
     'gatsby-plugin-postcss',
+    `gatsby-plugin-styled-components`,
     'gatsby-plugin-react-helmet',
     {
       resolve: 'gatsby-source-sanity',
